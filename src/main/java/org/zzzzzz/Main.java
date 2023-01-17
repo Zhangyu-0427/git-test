c08c65dc0ce84cc83c1d8634a31ed82b53e7d53b
zhangyu <1802833394@qq.com>
2023-01-17 22:05:30 +0800

    second commit

--- src/main/java/org/zzzzzz/Main.java
+++ src/main/java/org/zzzzzz/Main.java
@@ -3,5 +3,6 @@ package org.zzzzzz;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello world2!");
     }
 }
